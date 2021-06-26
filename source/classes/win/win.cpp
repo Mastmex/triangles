2225080dccaa0fb973dbd1da6cdaa28b474701af
Mast_mex <mastmex42@gmail.com>
2021-06-26 03:09:30 +0300

    first try

--- source/classes/win/win.cpp
+++ source/classes/win/win.cpp
@@ -1,7 +1,13 @@
 #include "win.hpp"
 
+win::win()
+{
+    this->heigth=1017;
+    this->length=1920;
+}
+
 void win::start(){
-    sf::RenderWindow window(sf::VideoMode(800,600),"xxx");
+    sf::RenderWindow window(sf::VideoMode(this->length,this->heigth),"triangles");
     while (window.isOpen())
     {
         // check all the window's events that were triggered since the last iteration of the loop
@@ -11,13 +17,29 @@ void win::start(){
             // "close requested" event: we close the window
             if (event.type == sf::Event::Closed)
                 window.close();
+            if (event.type == sf::Event::MouseButtonPressed)
+                {
+                    vec.push_back(sf::Mouse::getPosition());
+                    cout<<vec.back().x;
+                }
         }
 
         // clear the window with black color
         window.clear(sf::Color::Black);
 
         // draw everything here...
-        // window.draw(...);
+        if(vec.capacity()>=2)
+        {
+            for(int i=0;i<vec.capacity()-1;i++)
+                {
+                    sf::Vertex line[]=
+                    {
+                        sf::Vertex(sf::Vector2f(vec[i].x,vec[i].y)),
+                        sf::Vertex(sf::Vector2f(vec[i+1].x,vec[i+1].y))
+                    };
+                    window.draw(line, 2, sf::Lines);
+                }
+        }
 
         // end the current frame
         window.display();
--- source/classes/win/win.hpp
+++ source/classes/win/win.hpp
@@ -2,9 +2,19 @@
 #define WIN_HPP
 
 #include <SFML/Graphics.hpp>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+
+
 
 class win{
+private:
+int heigth,length;
+vector<sf::Vector2i> vec;
 public:
+    win();
     void start();
 };
 
